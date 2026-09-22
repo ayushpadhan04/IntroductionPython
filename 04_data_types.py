aba9535f88fcc7e10ae760623768bb39a156d2cd
ayushpadhan04 <ayushpadhan39@gmail.com>
2026-09-22 20:07:24 +0530

    Add Python data types example

---
+++ 04_data_types.py
@@ -0,0 +1,11 @@
+# Python Data Types
+
+name = "Ayush"       # String
+age = 21              # Integer
+height = 6.0          # Float
+student = True        # Boolean
+
+print("Name:", name)
+print("Age:", age)
+print("Height:", height)
+print("Student:", student)
