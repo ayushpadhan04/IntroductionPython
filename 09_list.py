5127d5e9a2c3988871a47c3aae83dd5143367f9b
ayushpadhan04 <ayushpadhan39@gmail.com>
2026-09-22 20:08:00 +0530

    Add Python list example

---
+++ 09_list.py
@@ -0,0 +1,10 @@
+# Python List
+
+fruits = ["Apple", "Banana", "Mango"]
+
+print("Fruits:", fruits)
+print("First fruit:", fruits[0])
+
+# Add a new item
+fruits.append("Orange")
+print("Updated list:", fruits)
